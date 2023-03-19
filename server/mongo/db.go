a1b73e0298f49ffbffe8d23b16805b2fc5ba016c
Alexander <wintersalex@icloud.com>
2023-03-19 15:53:26 -0700

    Add db.go to connect to the database

---
+++ server/mongo/db.go
@@ -0,0 +1,30 @@
+package mongo
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func init() {
+	// connect to the database
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	var err error
+	client, err = mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	// check the connection
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetMongoClient() *mongo.Client {
+	return client
+}
