e7bb0a74bdd6183348b17ed14bb1b6ecc53e849b
Alexander <wintersalex@icloud.com>
2023-03-19 19:16:03 -0700

    Restructure project Add routes and users.go for user functions Rename structs.go to models.go Add methods to server.go

---
+++ server/routes/users.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+// CreateUser inserts a new user into the database
+func CreateUser(c *fiber.Ctx) error {
+	// Your implementation here
+}
+
+// GetUser retrieves a user from the database by ID
+func GetUser(c *fiber.Ctx) error {
+	// Your implementation here
+}
+
+// UpdateUser updates a user in the database by ID
+func UpdateUser(c *fiber.Ctx) error {
+	// Your implementation here
+}
+
+// DeleteUser deletes a user from the database by ID
+func DeleteUser(c *fiber.Ctx) error {
+	// Your implementation here
+}
+
+// ListUsers retrieves all users from the database
+func ListUsers(c *fiber.Ctx) error {
+	// Your implementation here
+}
--- server/server.go
+++ server/server.go
@@ -5,25 +5,49 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+
+	"SENG468-A2/mongo"
 )
 
 func main() {
 	// Initialize a new Fiber app
 	app := fiber.New()
 
-	// // Connect to MongoDB
-	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	// defer cancel()
-
-	// client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017/seng468_a2_db"))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// Connect to MongoDB
+	client := mongo.GetMongoClient()
 
 	// Set up a simple route
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	// Set up the routes for users
+	app.Post("/api/users", CreateUser)
+	app.Get("/api/users/:id", GetUser)
+	app.Put("/api/users/:id", UpdateUser)
+	app.Delete("/api/users/:id", DeleteUser)
+	app.Get("/api/users", ListUsers)
+
+	// Set up the routes for posts
+	app.Post("/api/posts", CreatePost)
+	app.Get("/api/posts/:id", GetPost)
+	app.Put("/api/posts/:id", UpdatePost)
+	app.Delete("/api/posts/:id", DeletePost)
+	app.Get("/api/posts", ListPosts)
+
+	// Set up the routes for comments
+	app.Post("/api/comments", CreateComment)
+	app.Get("/api/comments/:id", GetComment)
+	app.Put("/api/comments/:id", UpdateComment)
+	app.Delete("/api/comments/:id", DeleteComment)
+	app.Get("/api/comments", ListComments)
+
+	// Set up the routes for notifications
+	app.Post("/api/notifications", CreateNotification)
+	app.Get("/api/notifications/:id", GetNotification)
+	app.Put("/api/notifications/:id", UpdateNotification)
+	app.Delete("/api/notifications/:id", DeleteNotification)
+	app.Get("/api/notifications", ListNotifications)
 
 	// Start the server on the specified port
 	port := os.Getenv("PORT")
