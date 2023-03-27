0c90df0b54b05302c4c1f29bc413c94f3f339752
Alexander <wintersalex@icloud.com>
2023-03-27 12:07:18 -0700

    Update db.go to use container name instead of localhost Update users.go and remove_data.go to use redis container name instead of localhost

--- scripts/dbScripts/remove_data.go
+++ scripts/dbScripts/remove_data.go
@@ -14,7 +14,7 @@ var rdb *redis.Client
 
 func init() {
 	rdb = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "myredis-container:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
